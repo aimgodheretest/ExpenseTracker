72aefdfe1f9437b04afcbfc900c321be7e9f2c5d
Murli Kumar <aimgodhere@gmail.com>
2026-03-10 22:42:25 +0530

    Removed Backend folder from git tracking

--- Backend/routes/signupRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/auth/signup", (req, res) => {
-  const { name, email, password } = req.body;
-  res.json({ message: "User created successfully" });
-});
-
-module.exports = router;
