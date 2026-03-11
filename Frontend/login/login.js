20c4e5e33d333d6571f28100fdc0629d6b4056ef
Murli Kumar <aimgodhere@gmail.com>
2026-03-11 11:43:21 +0530

    implement login form

--- Frontend/login/login.js
+++ Frontend/login/login.js
@@ -1,29 +1,27 @@
-const form = document.getElementById("signupForm");
+const form = document.getElementById("loginForm");
 
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
 
-  const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   const userData = {
-    name,
     email,
     password,
   };
 
-  console.log(userData);
-
   try {
     const response = await axios.post(
-      "http://localhost:3000/auth/signup",
+      "http://localhost:3000/auth/login",
       userData,
     );
 
-    alert(response.data.message);
+    document.getElementById("message").innerText = "Login Successful";
+
     console.log(response.data);
   } catch (error) {
-    alert(`User with same email is already exists`);
+    document.getElementById("message").innerText =
+      error.response.data.message || "Login failed";
   }
 });
