b2c7c47f3308d49de8c019da2adea6983b9c6811
Murli Kumar <aimgodhere@gmail.com>
2026-08-08 13:01:27 +0530

    fix: adjust layout to ensure minimum screen height for MainLayout

--- Frontend/src/layouts/MainLayout.jsx
+++ Frontend/src/layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/layout/Navbar";
 
 function MainLayout({ children }) {
   return (
-    <div className="flex h-screen bg-zinc-950">
+    <div className="flex min-h-screen bg-zinc-950">
       <Sidebar />
 
       <div className="flex flex-1 flex-col">
