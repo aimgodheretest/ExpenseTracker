2aac943e3c97b01d7c1b2aece0a1ef4a7ef702bb
Murli Kumar <aimgodhere@gmail.com>
2026-08-11 22:53:24 +0530

    refactor: update server startup log format and remove unused config file

--- Backend/app.js
+++ Backend/app.js
@@ -33,6 +33,6 @@ app.use(reportRouter);
 
 connectDB().then(() => {
   app.listen(port, () => {
-    console.log(`Server is Running on port:http://localhost:${port}`);
+    console.log(`Server is Running on port ${port}`);
   });
 });
